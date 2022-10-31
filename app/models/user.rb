b8c058414492cbc4a71b5bda6582eb7b721ff3e5
Sean Ephraim <s.ephraim@sportradar.com>
2022-10-31 09:44:50 -0500

    Changed(User): Relationship to Referral

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,4 @@
 class User < ApplicationRecord
-  has_many :referrer_referrals, class_name: 'Referral', foreign_key: 'referrer_id'
-  has_many :referee_referrals, class_name: 'Referral', foreign_key: 'referee_id'
+  has_one :referrer, class_name: 'Referral', foreign_key: 'referee_id'
+  has_many :referees, class_name: 'Referral', foreign_key: 'referrer_id'
 end
