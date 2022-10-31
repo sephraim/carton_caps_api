079d268b2236d3c377eb7fa8ac6809d5b299f003
Sean Ephraim <s.ephraim@sportradar.com>
2022-10-31 09:44:25 -0500

    Added: Local dev console script

--- local_dev/migrate.sh
+++ local_dev/migrate.sh
@@ -6,6 +6,3 @@ set -eu
 
 # Create the database (including schema and seed data)
 docker compose run --rm web wait-for-it --strict --timeout=30 db:3306 -- bundle exec rails db:migrate
-
-# Stop remaining service(s) & destroy containers
-docker compose down
