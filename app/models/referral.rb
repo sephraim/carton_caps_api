4d6e625c03a0800fd12aa9c93ddd470362d85e7b
Sean Ephraim <s.ephraim@sportradar.com>
2022-11-02 01:34:46 -0500

    Changed: Automatically create unique referral code for new users

--- app/models/referral.rb
+++ app/models/referral.rb
@@ -1,4 +1,10 @@
 class Referral < ApplicationRecord
+  before_create :set_completed_at_time
+
   belongs_to :referrer, class_name: 'User'
   belongs_to :referee, class_name: 'User'
+
+  def set_completed_at_time
+    self.completed_at = Time.now
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,19 @@
 class User < ApplicationRecord
+  REFERRAL_CODE_LENGTH = 6
+  private_constant :REFERRAL_CODE_LENGTH
+
+  before_create :create_unique_referral_code
+
   # NOTE: if a user gets destroyed then all its referrals also get destroyed
   has_one :referrer, class_name: 'Referral', foreign_key: 'referee_id', dependent: :destroy
   has_many :referees, class_name: 'Referral', foreign_key: 'referrer_id', dependent: :destroy
+
+  private
+
+  def create_unique_referral_code
+    loop do
+      self.referral_code = rand(36**REFERRAL_CODE_LENGTH).to_s(36).upcase
+      break unless self.class.exists?(referral_code: referral_code)
+    end
+  end
 end
